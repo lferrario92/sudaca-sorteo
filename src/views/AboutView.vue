e4fc19043cb5b407d776708f2a77babe423cd553
lferrario <lferrario@kiusys.com>
2023-03-22 13:46:18 -0300

    fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>About</h1>
     <p>
-      hola que tal. Aguante san lorenzo
+      hola que tal. Aguante San Lorenzo
     </p>
     <p>
       Creditos a Victor MV que me dio la idea de hacer esta cosa y a Promiedos que es de donde saque todas las imagenes de los clubes y las banderitas
